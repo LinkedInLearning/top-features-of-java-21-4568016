ca716fe57c30144e97c483c884eac78a140e28d6
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-05-05 15:53:27 +0200

    Add begin state for 04_01

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -13,11 +13,16 @@ public class Main {
     }
 
     private static void printDetails(Item item) {
-        if(item instanceof FoodItem(var name, var price)) {
-            System.out.println(STR."Food item with name \{name} has price \{price}");
-        } else if(item instanceof DrinkItem(String name, double price)) {
-            System.out.println(
-                    STR."Drink item with name \{name} has price \{price}");
+        if(item instanceof FoodItem) {
+            var foodItem = (FoodItem) item;
+            var name = foodItem.name();
+            var price = foodItem.price();
+            System.out.println("Food item with name " + name + " has price " + price);
+        } else if(item instanceof DrinkItem) {
+            var drinkItem = (DrinkItem) item;
+            var name = drinkItem.name();
+            var price = drinkItem.price();
+            System.out.println("Drink item with name " + name + " has price " + price);
         }
     }
 
