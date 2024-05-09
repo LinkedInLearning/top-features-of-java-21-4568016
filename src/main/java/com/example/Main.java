d399e76fc8fb4936edb1ddbbd913185d4417f209
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-05-09 10:57:47 +0200

    Add end state for 06_01

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,18 +5,13 @@ public class Main {
     public static void main(String[] args) {
 
         var house = new House("123 Main St", 3, true);
-        var store = new Store("456 Elm St");
-
-        printAddress(house);
-        printAddress(store);
+        printNumberOfBedrooms(house);
 
     }
 
-    private static void printAddress(Building building) {
-        if (building instanceof House house) {
-            System.out.println("House address: " + house.address());
-        } else if (building instanceof Store store) {
-            System.out.println("Store address: " + store.address());
+    private static void printNumberOfBedrooms(Building building) {
+        if (building instanceof House(_, int numberOfBedrooms, _)) {
+            System.out.println("Number of bedrooms: " + numberOfBedrooms);
         }
     }
 
