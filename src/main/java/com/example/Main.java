1bd66fa5a82fd07dde156fb3b9a4d8b5345cd28b
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-05-19 21:06:06 +0200

    Add begin state for 08_02

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,51 +1,23 @@
 package com.example;
 
-import java.math.BigDecimal;
-import java.util.ArrayDeque;
-import java.util.List;
-import java.util.Queue;
+import java.time.Duration;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<Item> items = List.of(
-                new Item(new BigDecimal("10.00")),
-                new Item(new BigDecimal("20.00")),
-                new Item(new BigDecimal("30.00")),
-                new Item(new BigDecimal("40.00")),
-                new Item(new BigDecimal("50.00"))
-        );
-
-        var basketTotal = 0;
-
-        for (Item _ : items) {
-            basketTotal ++;
+        try (var executor = Executors.newFixedThreadPool(1)) {
+            IntStream.range(0, 99999).forEach(i -> {
+                executor.submit(() -> {
+                    Thread.sleep(Duration.ofSeconds(1));
+                    System.out.println(i);
+                    return i;
+                });
+            });
         }
-
-        System.out.println("Basket total: " + basketTotal);
-
-
-        Queue<Item> items2 = new ArrayDeque<>(List.of(
-                new Item(new BigDecimal("10.00")),
-                new Item(new BigDecimal("20.00")),
-                new Item(new BigDecimal("30.00")),
-                new Item(new BigDecimal("40.00")),
-                new Item(new BigDecimal("50.00"))
-        ));
-
-        var basketTotal2 = 0;
-
-        for (Item item : items2) {
-            basketTotal2 ++;
-        }
-
-        System.out.println("Basket 2 total: " + basketTotal2);
-
-
 
     }
-
-
 
 }
