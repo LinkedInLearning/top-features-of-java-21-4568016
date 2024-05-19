3a0eca730e4d06c97ee1f67873175e59203f2ab3
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-05-19 14:28:32 +0200

    Add end state for 06_02

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,9 @@
 package com.example;
 
 import java.math.BigDecimal;
+import java.util.ArrayDeque;
 import java.util.List;
+import java.util.Queue;
 
 public class Main {
 
@@ -10,16 +12,36 @@ public class Main {
         List<Item> items = List.of(
                 new Item(new BigDecimal("10.00")),
                 new Item(new BigDecimal("20.00")),
-                new Item(new BigDecimal("30.00"))
+                new Item(new BigDecimal("30.00")),
+                new Item(new BigDecimal("40.00")),
+                new Item(new BigDecimal("50.00"))
         );
 
         var basketTotal = 0;
 
-        for (Item item : items) {
+        for (Item _ : items) {
             basketTotal ++;
         }
 
         System.out.println("Basket total: " + basketTotal);
+
+
+        Queue<Item> items2 = new ArrayDeque<>(List.of(
+                new Item(new BigDecimal("10.00")),
+                new Item(new BigDecimal("20.00")),
+                new Item(new BigDecimal("30.00")),
+                new Item(new BigDecimal("40.00")),
+                new Item(new BigDecimal("50.00"))
+        ));
+
+        var basketTotal2 = 0;
+
+        for (Item item : items2) {
+            basketTotal2 ++;
+        }
+
+        System.out.println("Basket 2 total: " + basketTotal2);
+
 
 
     }
