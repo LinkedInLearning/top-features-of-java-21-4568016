7eacc1c10244d488a53f88ef6518ed222cfbda8a
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-05-20 16:34:40 +0200

    Add 08_03

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,20 +2,26 @@ package com.example;
 
 import java.time.Duration;
 import java.util.concurrent.Executors;
-import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            IntStream.range(0, 99999).forEach(i -> {
-                executor.submit(() -> {
-                    Thread.sleep(Duration.ofSeconds(1));
-                    System.out.println(i);
-                    return i;
+        try (var executor = Executors.newSingleThreadExecutor()) {
+            int i = 0;
+            while (i < 10_000) {
+                executor.execute(() -> {
+                    try {
+                        System.out.println("Starting to execute task");
+                        Thread.sleep(Duration.ofSeconds(1));
+                        System.out.println("Task executed");
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
                 });
-            });
+                i++;
+            }
         }
 
     }
