f9eca7a40a509d93d95dab8a99c4d0cf609af9f7
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-05-26 21:57:28 +0200

    Add end state for 06_02

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,18 +7,27 @@ public class Main {
     public static void main(String[] args) {
 
         List<String> items = List.of(
-                "Item 1",
-                "Item 2",
-                "Item 3"
+                "1",
+                "2",
+                "3"
         );
 
         var basketTotal = 0;
 
-        for (String item : items) {
+        for (String _ : items) {
             basketTotal ++;
         }
 
         System.out.println("Basket total: " + basketTotal);
+
+        var lastItem = items.getLast();
+        try {
+            int i = Integer.parseInt(lastItem);
+            System.out.println("Last item: " + i);
+        } catch (NumberFormatException _) {
+            System.out.println("Bad number: " + lastItem);
+        }
+
 
 
     }
