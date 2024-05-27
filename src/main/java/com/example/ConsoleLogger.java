c826a20819aa3509425b1c2b5c4d1f64bdda5966
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-05-27 21:41:00 +0200

    Add 05_02

---
+++ src/main/java/com/example/ConsoleLogger.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class ConsoleLogger implements Logger {
+
+    @Override
+    public String getLoggerType() {
+        return "Console";
+    }
+
+}
---
+++ src/main/java/com/example/FileLogger.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class FileLogger implements Logger {
+
+    @Override
+    public String getLoggerType() {
+        return "File";
+    }
+
+}
