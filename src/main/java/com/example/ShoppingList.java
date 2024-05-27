135a8bece34e657ea8c62d272fb0e0cfe27514fe
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-05-27 18:52:44 +0200

    Add 03_02

---
+++ src/main/java/com/example/ShoppingList.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingList {
+
+    private ArrayList<String> list = new ArrayList<>(List.of("Bread", "Milk", "Eggs", "Butter"));
+
+    public String getFirstItem() {
+        return "";
+    }
+
+    public String getLastItem() {
+        return "";
+    }
+
+}
