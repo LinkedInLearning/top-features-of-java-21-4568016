e3a5ef11bb0626b050c7f0c12f0c620e4be96639
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-05-27 20:54:22 +0200

    Add end state for 04_03

--- src/main/java/com/example/TicketMachine.java
+++ src/main/java/com/example/TicketMachine.java
@@ -3,7 +3,13 @@ package com.example;
 public class TicketMachine {
 
     public String getDetails(Ticket ticket) {
-        return "Unknown ticket type";
+        if (ticket instanceof AdultTicket(double price)) {
+            return "Adult ticket has price " + price;
+        } else if (ticket instanceof ChildTicket(double price)) {
+            return "Child ticket has price " + price;
+        } else {
+            return "Unknown ticket type";
+        }
     }
 
 }
