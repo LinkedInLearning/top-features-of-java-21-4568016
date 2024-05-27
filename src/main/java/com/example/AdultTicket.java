3621955c9471758ce2a1dd4078bf290d75dfd2a4
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-05-27 20:38:05 +0200

    Add 04_02

---
+++ src/main/java/com/example/AdultTicket.java
@@ -0,0 +1,5 @@
+package com.example;
+
+public record AdultTicket(double price) implements Ticket {
+
+}
---
+++ src/main/java/com/example/ChildTicket.java
@@ -0,0 +1,5 @@
+package com.example;
+
+public record ChildTicket(double price) implements Ticket {
+
+}
