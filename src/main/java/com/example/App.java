db76edc5360f46036b7fa3df49ada2260f0164bf
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-05-28 20:13:55 +0200

    Add end state for 05_03

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,15 +3,18 @@ package com.example;
 public class App {
 
     public String getLoggerType(Logger logger) {
-        if (logger instanceof FileLogger fileLogger) {
-            System.out.println("Logger type is file");
-            return fileLogger.getLoggerType();
-        } else if (logger instanceof ConsoleLogger consoleLogger) {
-            System.out.println("Logger type is console");
-            return consoleLogger.getLoggerType();
-        } else {
-            throw new IllegalArgumentException("Logger type not supported");
+        switch (logger) {
+            case FileLogger fileLogger -> {
+                System.out.println("Logger type is file");
+                return fileLogger.getLoggerType();
+            }
+            case ConsoleLogger consoleLogger -> {
+                System.out.println("Logger type is cloud console");
+                return consoleLogger.getLoggerType();
+            }
+            default -> throw new IllegalArgumentException("Logger type not supported");
         }
+
 
     }
 
