f9336fcbbdb9a8eef4ce70195428a456d40ca310
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-05-28 21:03:56 +0200

    Add end state for 06_04

--- src/main/java/com/example/Register.java
+++ src/main/java/com/example/Register.java
@@ -4,14 +4,20 @@ import java.util.List;
 
 public class Register {
 
-    public List<DirectReport> printDirectReports(Employee employee) {
-        if (employee instanceof Manager manager) {
-            var directReports = manager.directReports();
-            System.out.println("Direct reprots: " + directReports);
-            return directReports;
+    public static void main(String[] args) {
+        var directReport1 = new DirectReport(1);
+        var directReport2 = new DirectReport(2);
+        var manager = new Manager(3, List.of(directReport1, directReport2));
+        printDirectReports(manager);
+    }
+
+    public static void printDirectReports(Employee employee) {
+        if (employee instanceof Manager(_, var directReports)) {
+            System.out.println("Direct reports: " + directReports);
         } else {
             throw new IllegalArgumentException("Employee is not a manager");
         }
     }
 
 }
+
