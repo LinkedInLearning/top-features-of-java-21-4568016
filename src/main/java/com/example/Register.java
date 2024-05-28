4cb3db943c867a6c725d9ed2f98a5cd6ab8d3840
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-05-28 20:46:00 +0200

    Add 06_03

---
+++ src/main/java/com/example/Register.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import java.util.List;
+
+public class Register {
+
+    public static void main(String[] args) {
+        var directReport1 = new DirectReport(1);
+        var directReport2 = new DirectReport(2);
+        var manager = new Manager(3, List.of(directReport1, directReport2));
+        printDirectReports(manager);
+    }
+
+    public static void printDirectReports(Employee employee) {
+        if (employee instanceof Manager manager) {
+            var directReports = manager.directReports();
+            System.out.println("Direct reports: " + directReports);
+        } else {
+            throw new IllegalArgumentException("Employee is not a manager");
+        }
+    }
+
+}
